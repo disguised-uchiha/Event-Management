b82137995efae20f48e72aff5eecfcbcdef4b14f
HarshBoricha98 <harsh.boricha2015@gmail.com>
2020-05-20 23:48:39 +0530

    ejs added to index file

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Event Management Website UI
 This is just the front end/ UI for event management website<br>
-You can integrate with your own backend<br>
 see the website UI in action: https://freakyevents.netlify.app/
--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 
+const app = express();
+// creating a local card array for lorem purpose
+
+const cards = [{
+    cardImage: '/images/1.jpg',  
+},
+{
+    cardImage: '/images/1.jpg',  
+},
+{
+    cardImage: '/images/1.jpg',  
+},
+{
+    cardImage: '/images/1.jpg',  
+}]
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+    res.render('index',{ 
+        pageTitle: 'RCOE Events',
+        cards: cards
+    })
+});
 app.listen(3000);
