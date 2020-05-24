c07132c5e6675f63a69684085fb6e3dd296c93ad
HarshBoricha98 <harsh.boricha2015@gmail.com>
2020-05-25 04:35:44 +0530

    404 page added

--- app.js
+++ app.js
@@ -10,5 +10,8 @@ app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(homeRoute);
-
+app.use((req, res, next) => {
+    res.status(404);
+    res.render('404')
+});
 app.listen(3000);
