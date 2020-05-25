7844fce5ccec3492e75aa246625c3cec37f59eb9
HarshBoricha98 <harsh.boricha2015@gmail.com>
2020-05-25 05:44:47 +0530

    .env and mongoose added

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+
+//for .env
+require("dotenv").config();
+
+const mongoose = require("mongoose");
 
 const app = express();
+// My Routes
+const homeRoute = require("./routes/home-routes");
 
-const homeRoute = require('./routes/home-routes')
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(homeRoute);
 app.use((req, res, next) => {
-    res.status(404);
-    res.render('404')
+  res.status(404);
+  res.render("404");
 });
 app.listen(3000);
+mongoose.connect('mongodb+srv://harsh:${DB_PASSWORD}@cluster0-lfbed.mongodb.net/test?retryWrites=true&w=majority',)
